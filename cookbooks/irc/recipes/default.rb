49c81dddfdd51dae299814d67b52687714fcdca3
Leopoldo Cent <lcento@gmail.com>
2016-06-21 18:27:41 +0100

    Adding TDI cookbooks

---
+++ cookbooks/irc/recipes/default.rb
@@ -0,0 +1,25 @@
+#
+# Cookbook Name:: irc
+# Recipe:: default
+#
+# Copyright 2016, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+user 'tdi' do
+  action :create
+  comment "Test Driver Infrastructure"
+  home "/home/tdi"
+  manage_home true
+end
+
+directory '/home/tdi/.irssi' do
+  owner 'tdi'
+  group 'tdi'
+end
+
+cookbook_file '/home/tdi/.irssi/config' do
+  source 'irssi-config'
+  owner 'tdi'
+  group 'tdi'
+end
